c7837c990ee0c62330e877806ca05c9a16c202ff
KieronMoore <67976013+KieronMoore@users.noreply.github.com>
2020-07-14 14:36:15 -0500

    FirstCommit
    FirstCommit

---
+++ Kieron_M_ControlFlowExercise.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var seconds = 180
+
+for index in 1...seconds {
+    print(" \(index * 1)")
+}
+
+if seconds == 180 {
+    print("Done!")
+}
+
+if seconds <= 179 {
+    print("Not done!")
+}
+
